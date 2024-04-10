861966322314f9586ffb90e7ddafc6d3ebb263a6
Yarelis Martin <yarelis0martin@gmail.com>
2024-04-09 22:26:17 -0500

    added the update author

--- pages/author/new.js
+++ pages/author/new.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AuthorForm from '../../components/forms/AuthorForm';
 
 // TODO: create a reusable form to add/edit book and render in this view
 
 export default function AddAuthor() {
   return (
-    <div> add author form component goes here </div>
+    <AuthorForm />
   );
 }
