41c5af1e89c3c963425347323fe5ba20725206f3
Yarelis Martin <yarelis0martin@gmail.com>
2024-04-16 14:29:54 -0500

    finished

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -13,18 +13,41 @@ export default function ViewAuthor() {
 
   const { firebaseKey } = router.query;
 
-  useEffect(() => {
+  const getAuthorDetails = () => {
     viewAuthorDetails(firebaseKey).then(setAuthorDetail);
+  };
+  /* we want a useState when we are making api calls because when we
+  call an api then we need to set it so make a usestate for
+  every api call */
+
+  useEffect(() => {
+    getAuthorDetails();
+    console.warn(firebaseKey);
+    return () => {
+      setAuthorDetail([]);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [firebaseKey]);
 
   return (
     <div>
-      <AuthorCard key={authorDetail.firebaseKey} authorObj={authorDetail} onUpdate={setAuthorDetail} />
+      <AuthorCard key={authorDetail.firebaseKey} authorObj={authorDetail} onUpdate={() => router.push('/authors')} />
       <div className="d-flex flex-wrap">
         {authorDetail.books?.map((bookObj) => (
-          <BookCard key={bookObj.firebaseKey} bookObj={bookObj} onUpdate={setAuthorDetail} />
+          <BookCard key={bookObj.firebaseKey} bookObj={bookObj} onUpdate={getAuthorDetails} />
         ))}
       </div>
     </div>
   );
 }
+
+/* When it comes to onUpdate it's reffering to the prop in the inetial component
+it's prop type is a function so it's expecting a function.
+
+for authorcard onupdate is updating when user del a card and fav/unfav a card.
+we are passing author card the route to the authors page becaise when an author is deleted we should take the user back to the author page.
+it is also occuring when a user fav/unfav which isn't ideal.
+
+for the bookcard onupdate runs when the user is del a card.
+we are getting the author details because when we del a book we want the author details page to still show
+ */
--- pages/author/edit/[firebaseKey].js
+++ pages/author/edit/[firebaseKey].js
@@ -5,6 +5,8 @@ import { getSingleAuthor } from '../../../api/authorData';
 
 export default function EditAuthor() {
   const [editItem, setEditItem] = useState({});
+
+  /* we use this in order to grab data from the dynamic routes */
   const router = useRouter();
 
   const { firebaseKey } = router.query;
